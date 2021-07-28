f0c0b3f786630a484d5154adf42810181d15f8dd
Yanelis Lopez <yalope@microsoft.com>
2021-07-28 15:50:37 -0700

    Add 2 to names

--- test2.tf
+++ test2.tf
@@ -10,49 +10,55 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "vn"
+resource "azurerm_virtual_network" "example2" {
+  name                = "vn2"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "testrg2"
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "subnet"
+resource "azurerm_subnet" "example2" {
+  name                 = "subnet2"
   resource_group_name = "testrg2"
-  virtual_network_name = azurerm_virtual_network.example.name
+  virtual_network_name = azurerm_virtual_network.example2.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
-resource "azurerm_network_interface" "ni_linux" {
-  name                = "linux"
+resource "azurerm_network_interface" "ni_linux2" {
+  name                = "linux2"
   location            = "East US"
   resource_group_name = "testrg2"
   enable_accelerated_networking = false
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.example2.id
     private_ip_address_allocation = "Dynamic"
   }
 
 }
 
-resource "azurerm_network_interface" "ni_win" {
-  name                = "win"
+resource "azurerm_network_interface" "ni_win2" {
+  name                = "win2"
   location            = "East US"
   resource_group_name = "testrg2"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.example2.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource azurerm_network_security_group "bad_sg" {
+resource azurerm_network_watcher "network_watcher2" {
   location            = "East US"
-  name                = "nsg"
+  name                = "network-watcher2"
+  resource_group_name = "testrg2"
+}
+
+resource azurerm_network_security_group "bad_sg2" {
+  location            = "East US"
+  name                = "nsg2"
   resource_group_name = "testrg2"
 
   security_rule {
@@ -80,20 +86,20 @@ resource azurerm_network_security_group "bad_sg" {
   }
 }
 
-resource azurerm_network_watcher_flow_log "flow_log" {
+resource azurerm_network_watcher_flow_log "flow_log2" {
   enabled                   = false
-  network_security_group_id = azurerm_network_security_group.bad_sg.id
-  network_watcher_name      = azurerm_network_watcher.network_watcher.name
+  network_security_group_id = azurerm_network_security_group.bad_sg2.id
+  network_watcher_name      = azurerm_network_watcher.network_watcher2.name
   resource_group_name       = "testrg2"
-  storage_account_id        = azurerm_storage_account.example.id
+  storage_account_id        = azurerm_storage_account.example2.id
   retention_policy {
     enabled = false
     days    = 10
   }
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "sa${random_integer.rnd_int.result}"
+resource "azurerm_storage_account" "example2" {
+  name                     = "sa${random_integer.rnd_int.result}2"
   resource_group_name      = "testrg2"
   location                 = "East US"
   account_tier             = "Standard"
